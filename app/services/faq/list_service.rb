33452bb493c02ed688f099035a407506904a2d9c
Marcos Aurelio <marcosaurelio.rn@gmail.com>
2020-06-07 10:39:29 -0300

    Negrito e cor na aparencia do Help

--- app/services/faq/list_service.rb
+++ app/services/faq/list_service.rb
@@ -20,10 +20,10 @@ module FaqModule
         faqs = Faq.all
       end
  
-      response = "*Perguntas e Respostas* \n\n"
+      response = "**Perguntas e Respostas** \n\n"
       faqs.each do |f|
-        response += "*#{f.id}* - "
-        response += "*#{f.question}*\n"
+        response += "**#{f.id}** - "
+        response += "**#{f.question}**\n"
         response += "`#{f.answer}` \n"
         f.hashtags.each do |h|
           response += "_##{h.name}_ "
